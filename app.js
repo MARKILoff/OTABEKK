df74eb01d023bb900ae6035ddf8019c3f830a816
Markiloff <zuxriddinovsirojidin@gmail.com>
2021-11-17 14:43:25 +0500

    siroj

---
+++ app.js
@@ -0,0 +1,12 @@
+let options = {
+    strings: ['Sayfuloo...', 'Eeee','Sayfuloo...','Jiyan qalesan','tincmisan mazen', 'yaxshimi🖐🖐🖐',
+    'bugn kelasamiii kottt'],
+    typeSpeed: 60,
+    startDelay: 2000,
+    backSpeed: 50,
+    backDeplay: 50,
+    loop: true,
+    loopCount: Infinity,
+};
+
+let typed = new Typed('.element', options);
