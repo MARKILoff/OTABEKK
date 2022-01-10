c0c69da39d56bd373a31ffa4756f2c35c3629250
Markiloff <zuxriddinovsirojidin@gmail.com>
2022-01-10 19:07:06 +0500

    ddddd

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 let options = {
-    strings: ['Sayfuloo...', 'Eeee','Sayfuloo...','Jiyan qalesan','tincmisan ', 'mazen yaxshimi🖐🖐🖐',
-    'bugn kelasamiii kottt'],
+    strings: ["ssss"],
     typeSpeed: 60,
     startDelay: 2000,
     backSpeed: 50,
